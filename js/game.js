ecb2fc5f349730bca9b61b1c0a5a33a92dbeb635
Bhavjotsingh-code <69036276+Bhavjotsingh-code@users.noreply.github.com>
2020-09-15 18:58:44 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,51 @@
+class Game{
+    constructor(){}
+getState(){
+var gameStateref=database.ref('gameState')
+gameStateref.on("value",function(data){
+gameState=data.val()
+
+
+})
+}
+update(state){
+    database.ref('/').update({
+        gameState:state 
+    })
+}
+start(){
+    if (gameState == 0){
+        player=new Player()
+        player.getCount()
+        form=new Form()
+        form.display()
+    }
+
+ }
+ player(){
+ form.hide()
+ textSize(30)   
+ text("Game start",120,100)
+ Player.getPlayerInfo()
+ if (allPlayers!==undefined){
+     var pos=130
+     for(var plr in allPlayers){
+        if (plr === "player" + player.index)
+        fill("red")
+        else
+        fill("black");
+
+        pos=pos+20
+        textSize(15)
+        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,pos)
+        
+        
+     }  
+
+ }
+ if(keyIsDown(UP_ARROW) && player.index !== null){
+    player.distance =player.distance + 50
+    player.update();
+ }
+}
+}
